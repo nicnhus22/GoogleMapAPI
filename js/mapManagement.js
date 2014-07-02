ed8a66346c2428b6825d5d7f42c718a2e0e54b30
Nicolas Husser <nicolas.husser@gmail.com>
2014-07-02 14:28:06 -0400

    Adding bouncing marker and comments

--- js/mapManagement.js
+++ js/mapManagement.js
@@ -1,12 +1,22 @@
 function initialize(){
+	// Create variable for London coordinates
+	var londonPos = new google.maps.LatLng(51.508742,-0.120850)
 
 	var mapProp = {
-	  center:new google.maps.LatLng(51.508742,-0.120850),
+	  center:londonPos,
 	  zoom:15,
 	  mapTypeId:google.maps.MapTypeId.TERRAIN
 	  };
+	  
+	var marker=new google.maps.Marker({
+	  position:londonPos,
+	  animation:google.maps.Animation.BOUNCE
+	  });
 
+	// Create map
 	var map=new google.maps.Map(document.getElementById("googleMap"), mapProp);
+	// Add marker to it
+	marker.setMap(map);
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);
