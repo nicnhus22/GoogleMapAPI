6b25eafc07f2f98f31938fc4d4b0c1cc140d4110
Nicolas Husser <nicolas.husser@gmail.com>
2014-07-02 17:03:42 -0400

    Add json file with cities and loop through them to display markers

--- js/mapManagement.js
+++ js/mapManagement.js
@@ -1,24 +1,40 @@
 // URL TO ADDRESS API: http://maps.google.com/maps/api/geocode/json?address=[address here]&sens.
 
+var randomLoc = [new google.maps.LatLng(35.21657099999999,54.799075999999999)];
+
+for (var key in cities) {
+  if (cities.hasOwnProperty(key)) {
+    randomLoc.push(new google.maps.LatLng(cities[key].lat,cities[key].lon));
+  }
+}
+
+var markers = [];
+
 function initialize(){
-	// Create variable for London coordinates
-	var londonPos = new google.maps.LatLng(45.21657099999999,5.799075999999999)
+	// Create variable for Grenoble coordinates
+	var grenoblePos = new google.maps.LatLng(45.21657099999999,5.799075999999999)
 
 	var mapProp = {
-	  center:londonPos,
-	  zoom:15,
+	  center:grenoblePos,
+	  zoom:5,
 	  mapTypeId:google.maps.MapTypeId.TERRAIN
 	  };
-	  
-	var marker=new google.maps.Marker({
-	  position:londonPos,
-	  animation:google.maps.Animation.BOUNCE
-	  });
-
+	
+	randomLoc.forEach(function(loc){
+		markers.push(new google.maps.Marker({
+		  position:loc,
+		  animation:google.maps.Animation.BOUNCE
+	  }));
+	});
 	// Create map
 	var map=new google.maps.Map(document.getElementById("googleMap"), mapProp);
-	// Add marker to it
-	marker.setMap(map);
+	
+	// Add markers to it
+	markers.forEach(function(marker){
+		marker.setMap(map);
+		name:"City"
+	});
+
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);
