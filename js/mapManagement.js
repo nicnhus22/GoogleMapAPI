0141716ae0db9f129393c25b2644bdc3ed6831aa
Nicolas Husser <nicolas.husser@gmail.com>
2014-07-03 08:55:36 -0400

    Add event listeners on the map to test

--- js/mapManagement.js
+++ js/mapManagement.js
@@ -1,40 +1,32 @@
 // URL TO ADDRESS API: http://maps.google.com/maps/api/geocode/json?address=[address here]&sens.
 
-var randomLoc = [new google.maps.LatLng(35.21657099999999,54.799075999999999)];
+function initialize() {
+  var mapOptions = {
+    zoom: 4,
+    center: new google.maps.LatLng(-25.363882, 131.044922)
+  };
 
-for (var key in cities) {
-  if (cities.hasOwnProperty(key)) {
-    randomLoc.push(new google.maps.LatLng(cities[key].lat,cities[key].lon));
-  }
-}
+  var map = new google.maps.Map(document.getElementById('googleMap'),
+      mapOptions);
 
-var markers = [];
+  var marker = new google.maps.Marker({
+    position: map.getCenter(),
+    map: map,
+    title: 'Click to zoom'
+  });
 
-function initialize(){
-	// Create variable for Grenoble coordinates
-	var grenoblePos = new google.maps.LatLng(45.21657099999999,5.799075999999999)
+  google.maps.event.addListener(map, 'center_changed', function() {
+    // 3 seconds after the center of the map has changed, pan back to the
+    // marker.
+    window.setTimeout(function() {
+      map.panTo(marker.getPosition());
+    }, 3000);
+  });
 
-	var mapProp = {
-	  center:grenoblePos,
-	  zoom:5,
-	  mapTypeId:google.maps.MapTypeId.TERRAIN
-	  };
-	
-	randomLoc.forEach(function(loc){
-		markers.push(new google.maps.Marker({
-		  position:loc,
-		  animation:google.maps.Animation.BOUNCE
-	  }));
-	});
-	// Create map
-	var map=new google.maps.Map(document.getElementById("googleMap"), mapProp);
-	
-	// Add markers to it
-	markers.forEach(function(marker){
-		marker.setMap(map);
-		name:"City"
-	});
-
+  google.maps.event.addListener(marker, 'click', function() {
+    map.setZoom(8);
+    map.setCenter(marker.getPosition());
+  });
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);
